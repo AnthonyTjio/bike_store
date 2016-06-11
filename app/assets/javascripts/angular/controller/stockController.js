10ac362fd1ed92b030705d1c0af92d4a06fc4c2a
KenToshi <anthony.tjio@outlook.co.id>
2016-06-11 04:23:56 -0700

    please be good

--- app/assets/javascripts/angular/controller/stockController.js
+++ app/assets/javascripts/angular/controller/stockController.js
@@ -1,12 +1,8 @@
 myApp.controller('stockController', ['$scope', '$http', function($scope, $http){
-<<<<<<< HEAD
   $scope.title = "Stocks";
-=======
-  $scope.title = "TEST";
   $scope.totalQty = function(){
   
   }
->>>>>>> d51b701324195a510afad3b7cdb2923fd78963b6
   $http.get(localhost+"/stocks.json").then(function(response,status,headers,config){
     $scope.stocks = response.data;
   },function(data,status,headers, config){
